25aaaa96c38eaa999ecfe0963a759d93ef95226d
gabrielOliv1 <gabrieloliveiravicente2015@gmail.com>
2023-08-24 14:21:18 -0300

    build: initial Java setup configuration

---
+++ src/main/java/com/gamix/GamixApplication.java
@@ -0,0 +1,13 @@
+package com.gamix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamix/gamix/GamixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamix.gamix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
