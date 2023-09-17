1ee043b6818d40a4446fd912e749a5e9b156671b
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-17 12:50:54 -0300

    feat: token controller added
    Co-authored-by: gabrielOliv1 <gabrielOliv1@users.noreply.github.com>

---
+++ src/main/java/com/gamix/controller/TokenController.java
@@ -0,0 +1,23 @@
+package com.gamix.controller;
+
+import com.gamix.models.PasswordUser;
+import com.gamix.security.JwtGenerator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class TokenController {
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final PasswordUser jwtUser) {
+        return jwtGenerator.generate(jwtUser);
+    }
+}
