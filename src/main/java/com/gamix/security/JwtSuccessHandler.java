66a32f0648e0f76dbe98ed93a8401e8eed0c0ac7
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-17 12:37:44 -0300

    feat: Sucess Handler maked
    Co-authored-by: gabrielOliv1 <gabrielOliv1@users.noreply.github.com>

--- src/main/java/com/gamix/security/JwtSuccessHandler.java
+++ src/main/java/com/gamix/security/JwtSuccessHandler.java
@@ -1,5 +1,19 @@
 package com.gamix.security;
 
-public class JwtSuccessHandler {
-    
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtSuccessHandler implements AuthenticationSuccessHandler{
+    @Override
+    public void onAuthenticationSuccess(
+        HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, 
+        Authentication authentication
+    ) throws IOException, ServletException {
+        System.out.println("Successfully Authentication");
+    }
 }
