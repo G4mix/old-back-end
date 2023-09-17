6d9e7539d8c44acedf47248376f9e0e5727d47d0
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-17 12:40:56 -0300

    feat: filter of the token configured
    Co-authored-by: gabrielOliv1 <gabrielOliv1@users.noreply.github.com>

--- src/main/java/com/gamix/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/gamix/security/JwtAuthenticationTokenFilter.java
@@ -1,5 +1,48 @@
 package com.gamix.security;
 
-public class JwtAuthenticationTokenFilter {
-    
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import com.gamix.models.JwtAuthenticationToken;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
+
+    public JwtAuthenticationTokenFilter() {
+        super("/graphql/**");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(
+        jakarta.servlet.http.HttpServletRequest request,
+        jakarta.servlet.http.HttpServletResponse response
+    ) throws AuthenticationException, IOException, jakarta.servlet.ServletException {
+        String header = request.getHeader("Authorization");
+
+
+        if (header == null || !header.startsWith("Token ")) {
+            throw new RuntimeException("JWT Token is missing");
+        }
+
+        String authenticationToken = header.substring(6);
+
+        JwtAuthenticationToken token = new JwtAuthenticationToken(authenticationToken);
+        return getAuthenticationManager().authenticate(token);
+    }
+
+    @Override
+	protected void successfulAuthentication(
+        HttpServletRequest request, HttpServletResponse response, 
+        FilterChain chain, Authentication authResult
+    ) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+        chain.doFilter(request, response);
+    }
 }
