d8e779ed7af4120cd716bc92642d5aa37cc40102
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-17 11:32:45 -0300

    feat: JwtValidator maked
    Co-authored-by: gabrielOliv1 <gabrielOliv1@users.noreply.github.com>

---
+++ src/main/java/com/gamix/security/JwtValidator.java
@@ -0,0 +1,41 @@
+package com.gamix.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.gamix.models.PasswordUser;
+import com.gamix.utils.Role;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class JwtValidator {
+    @Autowired
+    private Dotenv dotenv;
+
+    private String secret = dotenv.get("JWT_SIGNING_KEY_SECRET");
+
+    public PasswordUser validate(String token) {
+
+        PasswordUser jwtUser = null;
+        try {
+            Claims body = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            jwtUser = new PasswordUser();
+
+            jwtUser.getUser().setUsername(body.getSubject());
+            jwtUser.setPassword((String) body.get("userId"));
+            jwtUser.setRole((Role) body.get("role"));
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return jwtUser;
+    }
+}
