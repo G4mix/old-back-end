d9932d4f50b7d4abf9ea63fdc639b5a65598ee57
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-17 13:38:07 -0300

    feat: JwtSecutiryConfig is now working

--- src/main/java/com/gamix/config/JwtSecurityConfig.java
+++ src/main/java/com/gamix/config/JwtSecurityConfig.java
@@ -1,17 +1,36 @@
 package com.gamix.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.Customizer;
 
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.gamix.security.JwtAuthenticationEntryPoint;
+import com.gamix.security.JwtAuthenticationProvider;
+import com.gamix.security.JwtAuthenticationTokenFilter;
+import com.gamix.security.JwtSuccessHandler;
+
+@EnableMethodSecurity(prePostEnabled = true)
 @Configuration
 @EnableWebSecurity
 public class JwtSecurityConfig {
+    @Autowired
+    private JwtAuthenticationProvider authenticationProvider;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint entryPoint;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
@@ -20,10 +39,41 @@ public class JwtSecurityConfig {
                 auth.anyRequest().authenticated();
             })
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .httpBasic(withDefaults())
+            .httpBasic(Customizer.withDefaults())
             .build();
-
     }
 
-    
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(Collections.singletonList(authenticationProvider));
+    }
+
+    @Bean
+    public JwtAuthenticationTokenFilter authenticationTokenFilter() {
+        JwtAuthenticationTokenFilter filter = new JwtAuthenticationTokenFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
+        return filter;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        http.csrf(Customizer.withDefaults())
+            .authorizeHttpRequests(
+                (authorize) -> authorize
+                    .requestMatchers("**/graphql/**")
+                    .permitAll() 
+            )
+            .exceptionHandling(
+                (exceptionHandling) -> exceptionHandling.authenticationEntryPoint(entryPoint)
+            )
+            .sessionManagement(
+                (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+
+        http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.headers((header) -> header.cacheControl(Customizer.withDefaults()));
+        System.out.println("/graphql patterns");
+
+    }
 }
