534915ade22f7c31910d5c6ef6fc3dca8ea87f36
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-09-18 16:09:53 -0300

    test: added Signup test

---
+++ src/test/java/com/gamix/service/AuthServiceTest/SignUp.java
@@ -0,0 +1,119 @@
+package com.gamix.service.AuthServiceTest;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.gamix.records.UserRecords.UserInput;
+import com.gamix.models.PasswordUser;
+import com.gamix.models.User;
+import com.gamix.repositories.UserRepository;
+import com.gamix.security.JwtGenerator;
+import com.gamix.service.AuthService;
+import com.gamix.utils.Role;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SignUp {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private JwtGenerator jwtGenerator;
+
+    @MockBean
+    private AuthService authService;
+
+    private UserInput userInput;
+
+    @Before
+    public void setUp() {
+        userInput = new UserInput("user1", "user1@example.com", "password1", "icon1");
+    }
+
+    @Test
+    public void signUpPasswordUser_UserDoesNotExist() {
+        configureUserDoesNotExist();
+
+        PasswordUser createdPasswordUser = authService.signUpPasswordUser(userInput);
+
+        assertNotNull(createdPasswordUser);
+    }
+
+    @Test
+    public void signUpPasswordUser_UserExistsWithoutPasswordUser() {
+        configureUserExistsWithoutPasswordUser();
+
+        PasswordUser createdPasswordUser = authService.signUpPasswordUser(userInput);
+
+        assertNotEquals(userInput.password(), createdPasswordUser.getPassword());
+    }
+
+    @Test
+    public void signUpPasswordUser_UserExistsWithPasswordUser() {
+        configureUserExistsWithPasswordUser();
+
+        PasswordUser createdPasswordUser = authService.signUpPasswordUser(userInput);
+
+        assertNull(createdPasswordUser);
+    }
+
+    private User createNewUser() {
+        User user = new User();
+        user.setUsername(userInput.username());
+        user.setEmail(userInput.email());
+        user.setIcon(userInput.icon());
+        return user;
+    }
+
+    private PasswordUser createNewPasswordUser() {
+        PasswordUser passwordUser = new PasswordUser();
+        passwordUser.setUser(createNewUser());
+        passwordUser.setRole(Role.USER.toString());
+        passwordUser.setPassword(new BCryptPasswordEncoder().encode(userInput.password()));
+        passwordUser.setVerifiedEmail(false);
+        passwordUser.setToken("token_here");
+        return passwordUser;
+    }
+
+    private void configureUserDoesNotExist() {
+        User newUser = createNewUser();
+        when(userRepository.findByEmail(userInput.email())).thenReturn(null);
+        when(authService.createUser(userInput)).thenReturn(newUser);
+    
+        PasswordUser newPasswordUser = createNewPasswordUser();
+        String token = "token_here";
+        when(jwtGenerator.generate(newPasswordUser)).thenReturn(token);
+    
+        when(authService.signUpPasswordUser(userInput)).thenReturn(newPasswordUser);
+    }
+    
+    
+    private void configureUserExistsWithoutPasswordUser() {
+        User existingUser = createNewUser();
+        when(userRepository.findByEmail(userInput.email())).thenReturn(existingUser);
+        
+        when(authService.createUser(userInput)).thenReturn(existingUser);
+        PasswordUser passwordUser = createNewPasswordUser();
+        existingUser.setPasswordUser(passwordUser);
+        when(authService.signUpPasswordUser(userInput)).thenReturn(passwordUser);
+    }
+
+    private void configureUserExistsWithPasswordUser() {
+        User existingUser = createNewUser();
+        PasswordUser existingPasswordUser = createNewPasswordUser();
+        existingUser.setPasswordUser(existingPasswordUser);
+        when(userRepository.findByEmail(userInput.email())).thenReturn(existingUser);
+        when(authService.signUpPasswordUser(userInput)).thenReturn(null);
+    }
+}
