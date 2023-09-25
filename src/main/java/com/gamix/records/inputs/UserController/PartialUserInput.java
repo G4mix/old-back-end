d9856315ffe39d15383dd704a84516e2cbc98ca3
gabrielOliv1 <gabrieloliveiravicente2015@gmail.com>
2023-09-25 08:43:54 -0300

    style: removing unnecessary comments

--- src/main/java/com/gamix/records/inputs/UserController/PartialUserInput.java
+++ src/main/java/com/gamix/records/inputs/UserController/PartialUserInput.java
@@ -1,11 +1,5 @@
 package com.gamix.records.inputs.UserController;
 
-/* public record PartialUserInput(String username, String icon) {
-    public PartialUserInput(PartialUserInput partialUserInput) {
-        this(partialUserInput.username(), partialUserInput.icon());
-    }
-}  */
-
 public class PartialUserInput {
     private String username;
     private String email;
