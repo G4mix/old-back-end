280225bd8cae88568ea64fe0e7b5c73c19732b1e
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-10-05 13:25:39 -0300

    fix: fixed times for expiration of tokens

--- src/main/java/com/gamix/enums/ExpirationTime.java
+++ src/main/java/com/gamix/enums/ExpirationTime.java
@@ -1,8 +1,7 @@
 package com.gamix.enums;
 
 public enum ExpirationTime {
-    ACCESS_TOKEN(60000), REFRESH_TOKEN(70000), REMEMBER_ME(80000);
-    //ACCESS_TOKEN(3600000), REFRESH_TOKEN(604800000), REMEMBER_ME(2592000000L);
+    ACCESS_TOKEN(3600000), REFRESH_TOKEN(86400000), REMEMBER_ME(2592000000L);
     private final long value;
 
     ExpirationTime(long value) {
