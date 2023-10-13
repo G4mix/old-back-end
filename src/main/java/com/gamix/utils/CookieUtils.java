c5df9c3e0ab1dbec0d096714be80239da2e142f1
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-10-13 13:16:22 -0300

    refactor: fast changes in cookie

--- src/main/java/com/gamix/utils/CookieUtils.java
+++ src/main/java/com/gamix/utils/CookieUtils.java
@@ -11,44 +11,33 @@ public class CookieUtils {
     public static Map<String, String> generateCookies(String accessToken, String refreshToken, CookieOptions options) {
         Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
         refreshTokenCookie.setPath("/");
-
         if (options.rememberMe()) {
             refreshTokenCookie.setMaxAge(30 * 24 * 60 * 60);
         } else {
             refreshTokenCookie.setMaxAge(24 * 60 * 60);
         }
+        refreshTokenCookie.setSecure(options.isSecure());
 
         Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
         accessTokenCookie.setPath("/");
         accessTokenCookie.setMaxAge(3600);
-        accessTokenCookie.setHttpOnly(options.isSecure());
+        accessTokenCookie.setSecure(options.isSecure());
 
         Map<String, String> cookieStrings = new HashMap<>();
-        cookieStrings.put("refreshToken", buildSetCookieString(refreshTokenCookie, options.isSecure()));
-        cookieStrings.put("accessToken", buildSetCookieString(accessTokenCookie, options.isSecure()));
+        cookieStrings.put("refreshToken", buildSetCookieString(refreshTokenCookie));
+        cookieStrings.put("accessToken", buildSetCookieString(accessTokenCookie));
 
         return cookieStrings;
     }
 
-    private static String buildSetCookieString(Cookie cookie, boolean isSecure) {
+    private static String buildSetCookieString(Cookie cookie) {
         StringBuilder cookieString = new StringBuilder();
         cookieString.append(cookie.getName()).append("=").append(cookie.getValue());
 
-        if (cookie.getPath() != null) {
-            cookieString.append("; Path=").append(cookie.getPath());
-        }
-
-        if (cookie.getMaxAge() >= 0) {
-            cookieString.append("; Max-Age=").append(cookie.getMaxAge());
-        }
-
-        if (cookie.isHttpOnly()) {
-            cookieString.append("; HttpOnly");
-        }
-    
-        if (isSecure) {
-            cookieString.append("; Secure");
-        }
+        if (cookie.getPath() != null) cookieString.append("; Path=").append(cookie.getPath());
+        if (cookie.getMaxAge() >= 0) cookieString.append("; Max-Age=").append(cookie.getMaxAge());
+        if (cookie.isHttpOnly()) cookieString.append("; HttpOnly");
+        if (cookie.getSecure()) cookieString.append("; Secure");
     
         cookieString.append("; SameSite=Lax");
 
