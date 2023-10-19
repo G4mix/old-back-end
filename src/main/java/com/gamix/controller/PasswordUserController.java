979f12f94081eff7b1ecf1adc1b2bd660cd8d016
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-10-19 14:45:08 -0300

    fit: fast clean

--- src/main/java/com/gamix/controller/PasswordUserController.java
+++ src/main/java/com/gamix/controller/PasswordUserController.java
@@ -45,7 +45,6 @@ public class PasswordUserController {
             Map<String, String> cookieStrings = CookieUtils.generateCookies(
                 jwtTokens, new CookieOptions(false, req.isSecure())
             );
-            cookieStrings.put("teste", "som");
     
             return ResponseEntity.status(HttpStatus.OK).body(cookieStrings);
         } catch(ExceptionBase ex) {
