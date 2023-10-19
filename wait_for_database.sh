44822b6ce1db0fd5ba22b73b41ca723a77b3b81b
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-10-19 17:57:42 -0300

    feat: direct command

--- wait_for_database.sh
+++ wait_for_database.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Script para aguardar a disponibilidade de um serviço TCP
-# Exemplo de uso:
-#   ./wait_database.sh host porta -t tempo_de_espera
-
 host=$1
 port=$2
 timeout=${3:-15}
 
-# Utilizando um loop while para esperar até que a conexão seja estabelecida
 while ! nc -v $host $port </dev/null; do
     echo "Esperando por $host:$port ..."
     sleep 1
