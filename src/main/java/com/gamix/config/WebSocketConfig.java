967d3549d5c5e9be548a874654d4bf32a4fbef7e
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-10-31 13:07:01 -0300

    feat: web socket configured

---
+++ src/main/java/com/gamix/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.gamix.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private Dotenv dotenv;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*", dotenv.get("FRONT_END_BASE_URL")).withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/", "/queue/");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+}
