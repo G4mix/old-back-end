af5775499b72dd648c48182126198c228b4074b5
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-11-17 22:53:38 -0300

    fit: trying to make the resolver

--- src/main/java/com/gamix/models/Comment.java
+++ src/main/java/com/gamix/models/Comment.java
@@ -1,8 +1,10 @@
 package com.gamix.models;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +13,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +28,12 @@ public class Comment {
     @ManyToOne(optional = true)
     @JoinColumn(name = "post_id")
     private Post post;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true)
+    private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
     private List<Like> likes = new ArrayList<>();
@@ -39,7 +49,18 @@ public class Comment {
     @JoinColumn(name = "parent_comment_id")
     private Comment parentComment;
 
+    @Column(nullable = false, length = 200)
     private String content;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     public String getContent() {
         return content;
@@ -63,5 +84,13 @@ public class Comment {
 
     public void setUserProfile(UserProfile userProfile) {
         this.userProfile = userProfile;
+    }
+
+    public LocalDateTime createdAt() {
+        return this.createdAt;
+    }
+
+    public LocalDateTime updatedAt() {
+        return this.updatedAt;
     }
 }
--- src/main/java/com/gamix/models/Post.java
+++ src/main/java/com/gamix/models/Post.java
@@ -1,15 +1,22 @@
 package com.gamix.models;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,12 +27,24 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @ManyToOne(optional = true)
     @JoinColumn(name = "user_profile_id")
     private UserProfile author;
-
+    
+    @Column(nullable = false, length = 70)
     private String title;
+
+    @Column(nullable = false, length = 700)
     private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = true)
+    private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     private List<View> views = new ArrayList<>();
@@ -35,6 +54,37 @@ public class Post {
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     private List<Comment> comments = new ArrayList<>();
+
+    public int calculateLikesCount() {
+        return entityManager.createQuery("SELECT COUNT(l) FROM Like l WHERE l.post = :post", Long.class)
+                .setParameter("post", this)
+                .getSingleResult()
+                .intValue();
+    }
+
+    public int calculateCommentsCount() {
+        return entityManager.createQuery("SELECT COUNT(c) FROM Comment c WHERE c.post = :post", Long.class)
+                .setParameter("post", this)
+                .getSingleResult()
+                .intValue();
+    }
+
+    public int calculateViewsCount() {
+        return entityManager.createQuery("SELECT COUNT(v) FROM View v WHERE v.post = :post", Long.class)
+                .setParameter("post", this)
+                .getSingleResult()
+                .intValue();
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     public UserProfile getAuthor() {
         return author;
@@ -58,5 +108,23 @@ public class Post {
     public String setContent(String content) {
         this.content = content;
         return content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt != null ? createdAt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : null;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt != null ? updatedAt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : null;
+    }
+
+    public List<Like> getLikes() {
+        return this.likes;
+    }
+    public List<Comment> getComments() {
+        return this.comments;
+    }
+    public List<View> getViews() {
+        return this.views;
     }
 }
