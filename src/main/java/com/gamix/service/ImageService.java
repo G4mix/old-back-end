78669c65e2420758ff4322f42690320b39f4607f
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-11-22 18:15:28 -0300

    feat: image upload

--- src/main/java/com/gamix/service/ImageService.java
+++ src/main/java/com/gamix/service/ImageService.java
@@ -1,32 +1,44 @@
 package com.gamix.service;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import com.gamix.models.Image;
 import com.gamix.models.Post;
-import com.gamix.utils.SingleMultipartFile;
 import jakarta.servlet.http.Part;
 
 @Service
-public class ImageService {
+public class ImageService {    
     public List<Image> createImagesForPost(Post post, List<Part> files) throws IOException {
+        String imagesFolder = "images/posts/"+post.getId()+"/";
         List<Image> images = new ArrayList<>();
         for (Part partFile : files) {
-            MultipartFile multipartFile = new SingleMultipartFile(partFile);
-            
-            BufferedImage bufferedImage = ImageIO.read(multipartFile.getInputStream());
+            String fileName = partFile.getSubmittedFileName();
+            InputStream fileContent = partFile.getInputStream();
+
+            long fileSizeInBytes = partFile.getSize();
+            long maxSizeBytes = 2 * 1024 * 1024;
+    
+            if (fileSizeInBytes > maxSizeBytes) {
+                continue;
+            }
+
+            String resourcesPath = getClass().getClassLoader().getResource("").getPath();
+            String imagesFolderPath = resourcesPath + imagesFolder;
+            String src = imagesFolderPath + fileName;
+            File imageFile = new File(src);
+
+            saveFile(fileContent, imageFile);
+
+            BufferedImage bufferedImage = ImageIO.read(imageFile);
             int width = bufferedImage.getWidth();
             int height = bufferedImage.getHeight();
-            String fileName = multipartFile.getOriginalFilename();
-            
-            String src = ""; // fazer upload do arquivo e pegar o src
-            
-            System.out.println(src);
 
             Image image = new Image();
             image.setName(fileName);
@@ -38,5 +50,15 @@ public class ImageService {
             images.add(image);
         }
         return images;
+    }
+
+    private void saveFile(InputStream fileContent, File file) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            byte[] buffer = new byte[2097152];
+            int bytesRead;
+            while ((bytesRead = fileContent.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        }
     }
 }
